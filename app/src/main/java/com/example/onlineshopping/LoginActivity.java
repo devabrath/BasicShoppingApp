388f8a2d6c79b1488af9664b3608a03b727291c1
devabrath <devabrathsahu7032@gmail.com>
2023-04-10 19:43:14 +0530

    done

--- app/src/main/java/com/example/onlineshopping/LoginActivity.java
+++ app/src/main/java/com/example/onlineshopping/LoginActivity.java
@@ -16,7 +16,7 @@ import android.widget.Toast;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView forgotPass;
     EditText email,pass;
     Button login;
     TextView reg;
@@ -34,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
         pass = (EditText)findViewById(R.id.editTextPassword);
         login = (Button)findViewById(R.id.LoginButton);
         reg = (TextView)findViewById(R.id.new_user);
+        forgotPass = (TextView)findViewById(R.id.forgotPassword);
 
         email_layout = (TextInputLayout)findViewById(R.id.textInputEmail);
         pass_layout = (TextInputLayout)findViewById(R.id.textInputPassword);
@@ -53,14 +54,33 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (!inputValidation.isInputEditTextFilled(email, email_layout, getString(R.string.error_message_email))) {
+                    return;
+                }
+                if (!inputValidation.isInputEditTextEmail(email, email_layout, getString(R.string.error_message_email))) {
+                    return;
+                }
+                if (!inputValidation.isInputEditTextFilled(pass, pass_layout, getString(R.string.error_message_email))) {
+                    return;
+                }
 
+                if (databaseHelper.checkUser(email.getText().toString().trim(),pass.getText().toString().trim())) {
 
-                Intent accountsIntent = new Intent(LoginActivity.this,Nav.class);
-                overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
-                startActivity(accountsIntent);
-
+                    Intent accountsIntent = new Intent(LoginActivity.this, Nav.class);
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+                    startActivity(accountsIntent);
+                }else {
+                    Toast.makeText(LoginActivity.this,getString(R.string.error_valid_email_password), Toast.LENGTH_SHORT).show();
+                }
             }
 
+        });
+
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this,"Feature will be \n Available Soon",Toast.LENGTH_SHORT).show();
+            }
         });
 
 
